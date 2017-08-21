cc9d0f6dc2167a42f4fb9c021c0bd3b42711f69b
elpaule <michael.paulini@wormbase.org>
2017-08-21 15:33:00 +0100

    don't delete the source files, as we use them as base for recompiling

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ cd /apollo/ && \
     ./apollo clean-all && ./apollo deploy && \
     cp /apollo/target/apollo*.war /tmp/apollo.war && \
 	# So we can remove ~1.6 GB of cruft from the image. Ignore errors because cannot remove parent dir /apollo/
-	rm -rf /apollo/ || true && \
+	# rm -rf /apollo/ || true && \
+        # -- will keep them to use for recompilation
 	# Before moving back into a standardized location (that we have write access to)
 	mv /tmp/apollo.war /apollo/apollo.war
